1d807a807c21a9f6c0d4f8562b7af5412327f678
kayque <kayque.original@hotmail.com>
2023-01-20 09:26:46 -0300

    Creating new template and adding gitignore file

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from app import app
+from flask import render_template
 
 @app.route('/')
 def home():
     return render_template('home/home.html')
 
 @app.route('/profile')
-def teste():
+def profile():
     return render_template('profile/profile.html')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
