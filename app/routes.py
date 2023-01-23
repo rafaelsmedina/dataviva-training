0caa11713956b92b4a54cf067710805f346f383c
Samuel Bernardes <sbernardes467@gmail.com>
2023-01-22 21:13:51 -0300

    create database and modules initial config insert new users and post class

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from app.modules.models import User
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('home/home.html')
+    user = User.query.filter_by(username="john").first()
+    print(user)
+    return render_template('home/home.html', username=user.username)
+
 
 @app.route('/profile')
 def profile():
     return render_template('profile/profile.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
