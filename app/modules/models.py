fe467e8eb46de747187e65b43f3cbc385318af66
João Vitor <joaovitorotoni6@gmail.com>
2023-01-23 09:39:12 -0300

    fix models

--- app/modules/models.py
+++ app/modules/models.py
@@ -1,11 +1,18 @@
 from app import db
 from datetime import date
+from hashlib import md5
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        about_me = db.Column(db.String(140))
+        last_seen = db.Column(db.DateTime, default=date.today())
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
